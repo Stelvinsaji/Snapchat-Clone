5d22e479383961c6f7f8e1c6bf9d5a09eed3988d
Stelvinsaji <87966154+Stelvinsaji@users.noreply.github.com>
2021-12-06 22:27:55 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,7 @@
 import firebase from 'firebase'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB61kPwrVb10a0iIYPaPWurncdUKs__lzY",
-  authDomain: "snapchat-clone-2ccdd.firebaseapp.com",
-  projectId: "snapchat-clone-2ccdd",
-  storageBucket: "snapchat-clone-2ccdd.appspot.com",
-  messagingSenderId: "1071788093796",
-  appId: "1:1071788093796:web:2689254062c2fae6db8a71"
+  // Paste your Config here
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig)
